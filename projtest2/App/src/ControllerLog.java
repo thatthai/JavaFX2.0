daa54ad9cb80fc849045ef74237d4c048bff38bf
Neeracha Lawmatjit <88836725+AD-XERLINE@users.noreply.github.com>
2022-05-02 22:12:00 +0700

    Add files via upload

---
+++ projtest2/App/src/ControllerLog.java
@@ -0,0 +1,28 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class ControllerLog {
+
+    @FXML
+    private Button ButtonLogin;
+
+    @FXML
+    private Button ButtonSingUp;
+
+    @FXML
+    private ImageView Logo;
+
+    @FXML
+    private ImageView MLElement1;
+
+    @FXML
+    private PasswordField PassId;
+
+    @FXML
+    private TextField UsernameId;
+
+}
+
